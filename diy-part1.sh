1c82c9074c71fcd15c40b96ad06b71706299c2ef
DeXtmL <DeXtmL@users.noreply.github.com>
2021-05-06 18:24:45 +0800

    Add passwall and ssrplus to feed packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# add passwall and ssrplus
+git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/kenzok8/small.git
